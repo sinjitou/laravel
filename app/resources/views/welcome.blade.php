d9b651d12d42f5f744f5d2b27be8deaa991c9cf3
sinjitou <valdolard@gmail.com>
2024-02-08 13:53:38 +0100

    final version before build

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -866,13 +866,15 @@
             </div>
             <div class="text-center">
                 <h1 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"> {{ __('auth.welcome') }} </h1>
-                <a class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
-                    href="{{ route('addpassword') }}">
-                    <h2
-                        class="text-center w-full font-semibold text-gray-600 dark:text-gray-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                        {{ __('addpwd.title') }}
-                    </h2>
-                </a>
+                @if (Auth::check())
+                    <a class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
+                        href="{{ route('addpassword') }}">
+                        <h2
+                            class="text-center w-full font-semibold text-gray-600 dark:text-gray-400 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                            {{ __('addpwd.title') }}
+                        </h2>
+                    </a>
+                @endif
             </div>
 
 
